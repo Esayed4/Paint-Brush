8bb027f1d539d3c87fcac8bd9f6b81ecec07f9be
Omar Ayman <96844139+maro222@users.noreply.github.com>
2026-01-05 16:29:24 +0200

    Update and rename PaintModeController.java to controllers/PaintModeController.java

--- controllers/PaintModeController.java
+++ controllers/PaintModeController.java
@@ -1,11 +1,7 @@
 package controllers;
-// import java.awt.BasicStroke;
-// import javax.swing.ImageIcon;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
-// import java.awt.Color;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import view.*;
 import shapes.*;
